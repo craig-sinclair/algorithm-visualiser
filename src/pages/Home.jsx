df0ce0164240e386a64283232d8a4f1bdfc49f8f
Craig Sinclair <2766838S@student.gla.ac.uk>
2024-11-28 16:39:58 +0000

    updates to home page styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,27 @@ const HomePage = () => {
 
     return(
         <>
-            <div className='title-container'>    
-                <h1>Graph Algorithm Visualiser</h1>
+            <div className='title-container'>
+                <h1>Algorithm Visualiser</h1>
 
-                <div className='buttons-container'>
-                    <Link to='/bfs'><button>Breadth-First Search (BFS)</button></Link>
-                    <Link to='/dfs'><button>Depth-First Search (DFS)</button></Link>
-                    <Link to='/bfs'><button>Dijkstra's Algorithm</button></Link>
-                    <Link to='/bfs'><button>Prim-Jarnik Algorithm</button></Link>
-                    <Link to='/kmp'><button>KMP String Search</button></Link>
+                <div className="algorithm-section main-section">
+                    <h3>Graph Algorithms:</h3>
+                    <div className='buttons-container'>
+                        <Link to='/bfs'><button>Breadth-First Search (BFS)</button></Link>
+                        <Link to='/dfs'><button>Depth-First Search (DFS)</button></Link>
+                        <Link to='/bfs'><button>Dijkstra's Algorithm</button></Link>
+                        <Link to='/bfs'><button>Prim-Jarnik Algorithm</button></Link>
+                    </div>
+                </div>
+
+                <div className="algorithm-section">
+                    <h3>Text Algorithms:</h3>
+                    <div className='buttons-container'>
+                        <Link to='/kmp'><button>KMP String Search</button></Link>
+                    </div>
                 </div>
             </div>
         </>
-
     )
 
 }
